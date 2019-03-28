efb01c1723774015a5a33f07bec80a12d80b1f41
Alexey Duryagin <aiduryagih@gmail.com>
2019-03-28 10:25:30 +0300

    send worklog with debounce

--- src/intents/development.ts
+++ src/intents/development.ts
@@ -1,3 +1,4 @@
+import debounce from 'lodash/debounce';
 import Task from '../core/task';
 
 const DEVELOPMENT_NAME = 'разработка';
@@ -7,11 +8,23 @@ class Development extends Task {
   name: string = DEVELOPMENT_NAME;
   type: string = TYPE;
 
+  debouncedSendWorkLog = debounce((agent: any) => {
+    this.end(agent);
+  }, 1000 * 60 * 5);
+
+  sendDevelopmentWorkLog(agent: any) {
+    if (!this.inProgress) this.start();
+
+    this.debouncedSendWorkLog(agent);
+    return agent.add('Ок');
+  }
+
   initIntents() {
     this.intentMap.set('Development', (agent: any) => this.takeInProgress(agent));
     this.intentMap.set('Development cancel', (agent: any) => this.cancel(agent));
     this.intentMap.set('How long is the development', (agent: any) => this.howLong(agent));
     this.intentMap.set('Development ends', (agent: any) => this.end(agent));
+    this.intentMap.set('Development in progress', (agent: any) => this.sendDevelopmentWorkLog(agent))
   }
 }
 
